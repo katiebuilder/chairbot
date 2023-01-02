f94bbe924cc6c59e520539a28b1c71c59dc5fd23
Keaton <69707286+keatonbuilder@users.noreply.github.com>
2023-01-02 15:33:42 -0600

    oops

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,7 +1,7 @@
 const { Events } = require('discord.js');
 
 module.exports = {
-	name: Events.InteractionCreate,
+	name: 'interactionCreate',
 	async execute(interaction) {
 		if (!interaction.isChatInputCommand()) return;
 
