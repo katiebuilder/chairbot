362c160de16c91a6bd42b655e5bcbddafd259d39
Keaton <69707286+keatonbuilder@users.noreply.github.com>
2023-01-08 20:19:50 -0600

    watching you.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Collection, Events, GatewayIntentBits, ActivityType } = require('discord.js');
 const { token } = require('./config.json');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
@@ -17,7 +17,7 @@ for (const file of commandFiles) {
 
 client.once(Events.ClientReady, () => {
 	console.log('Ready!');
-	client.user.setActivity('Alpha v0.0.2');
+	client.user.setActivity('you', { type: ActivityType.Watching });
 });
 
 client.on(Events.InteractionCreate, async interaction => {
